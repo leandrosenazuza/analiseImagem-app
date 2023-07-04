3e454ef7dd50eb628df01188da4e2bdb275e32e1
08786218654 <leandrosena@lampp-it.com.br>
2023-07-03 23:47:48 -0300

    fix

--- src/app/module/funcionalidades/dashboard/dashboard.component.ts
+++ src/app/module/funcionalidades/dashboard/dashboard.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { bubbleData } from './data/data';
 
 
 @Component({
@@ -9,47 +8,6 @@ import { bubbleData } from './data/data';
 })
 export class DashboardComponent{
 
-  bubbleData: any[] = [];
-  //view: any[] = [700, 400];
-  view: any[] = [700];
 
-
-
-  // options
-  showXAxis: boolean = true;
-  showYAxis: boolean = true;
-  gradient: boolean = false;
-  showLegend: boolean = true;
-  showXAxisLabel: boolean = true;
-  yAxisLabel: string = 'Sales';
-  showYAxisLabel: boolean = true;
-  xAxisLabel: string = 'Months';
-  maxRadius: number = 20;
-  minRadius: number = 5;
-  yScaleMin: number = 70;
-  yScaleMax: number = 85;
-
-  colorScheme = {
-    domain: ['#5AA454', '#A10A28', '#C7B42C', '#AAAAAA']
-  };
-
-  constructor() {
-    Object.assign(this, { bubbleData });
-  }
-
-  onSelect(data: any): void {
-    console.log('Item clicked', JSON.parse(JSON.stringify(data)));
-  }
-
-  onActivate(data: any): void {
-    console.log('Activate', JSON.parse(JSON.stringify(data)));
-  }
-
-  onDeactivate(data: any): void {
-    console.log('Deactivate', JSON.parse(JSON.stringify(data)));
-  }
-
-  ngOnInit(): void {
-  }
 
 }
